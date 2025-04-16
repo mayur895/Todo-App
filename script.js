2853473e218187d8b24d65f771a4e5e3abe142e0
Mayur Singh <officialmayur51@gmail.com>
2025-04-16 18:29:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ todoInput.addEventListener('keydown', (event) => {
         addTodoButton.click(); // Simulate click on Enter
     }
 });
-
+console.log("Hello");
 function addTodo() {
     const todoText = todoInput.value.trim();
     if (todoText) {
